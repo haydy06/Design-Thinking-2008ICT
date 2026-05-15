06edaed32d476a10cd43f5d3b5ae64fce06ba598
haydy06 <haydyrocks@hotmail.com>
2026-05-15 16:42:03 +1000

    i have added a graph using js, i have added a container to hold all elements we create (cards) and i have added a header for the nav bar

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 /* DO NOT MAKE EDITS OUTSIDE YOUR CODE BLOCK */
 
 /* Haydens code Block START */
+const Values = [10, 20, 30, 25, 50, 45, 60]
+const labels = ["Monday", "Tuesday", "Wednesday", "Thursday" ,"Friday", "Saturday", "Sunday"]
 
+new Chart("usageGraph", {
+    type: "line",
+    data: {
+        labels: labels,
+        datasets: [{
+            data: Values
+        }]
+    },
+    options: {
+        responsive: true,
+    }
+})
 /* Haydens code Block END */
 
 
